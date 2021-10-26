b3bda3fcc377e325eb19b33bbca0eebf91d5ccb4
Leonardo Gomes <leonardodasigomes@gmail.com>
2021-10-25 22:50:23 -0300

    Create introdutory modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+
+import { useState } from "react";
 import { ForceGraph3D } from "react-force-graph";
-import { Button } from "@material-ui/core";
+import { Button, Modal, Box } from "@material-ui/core";
 
 const myData = {
   nodes: [
@@ -33,10 +35,34 @@ const myData = {
 };
 
 const App = () => {
+  const [modal, setModal] = useState(true);
+
   return (
     <div className="App">
+      <Modal
+        open={modal}
+        onClose={() => setModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className="modal"
+      >
+        <Box className="box">
+          <h2>Graph Balancer</h2>
+          <p>
+            O objetivo deste trabalho é ser uma aplicação hipotética para o uso
+            de algoritmos para marcação de nós de um grafo com diferentes flags,
+            para este caso, cores. Entretanto, para aumentar a complexidade do
+            algoritmo utilizado, é imposta a restrição de que nós vizinhos não
+            podem ser marcados com a mesma cor. A priori isto parece ser uma
+            tarefa simples de ser resolvida, mas o desafio se torna interessante
+            quando temos um grafo grande e denso sendo colocado a prova nesta
+            aplicação.
+          </p>
+        </Box>
+      </Modal>
       <div className="SideBar">
         <div className="SideBarContent">
+          <h2>Graph Balancer</h2>
           <Button variant="outlined" className="button">
             Gerar grafo
           </Button>
